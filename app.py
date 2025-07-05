5018b13d66390e62509811991e735e7acf11c0ed
autofollower350 <followerauto350@gmail.com>
2025-07-06 02:45:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+#Install Chrome version 136
+
+!wget -q https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.113/linux64/chrome-linux64.zip !unzip -q chrome-linux64.zip !mv chrome-linux64 /opt/chrome !ln -sf /opt/chrome/chrome /usr/bin/google-chrome
+
+Download matching ChromeDriver v136
+
+!rm -f /usr/local/bin/chromedriver !wget -q -O chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.113/linux64/chromedriver-linux64.zip !unzip -o chromedriver.zip !mv chromedriver-linux64/chromedriver /usr/local/bin/chromedriver !chmod +x /usr/local/bin/chromedriver
+#Impor
 import os
 import time
 import asyncio
